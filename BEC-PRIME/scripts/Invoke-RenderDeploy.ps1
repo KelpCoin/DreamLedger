7691b7402f3df2513acf82d2718562ca35e606b1
KelpCoin <josharchibald89@gmail.com>
2026-08-28 17:24:06 +1200

    ops: add Render deploy entrypoint

---
+++ BEC-PRIME/scripts/Invoke-RenderDeploy.ps1
@@ -0,0 +1,34 @@
+#requires -Version 5.1
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$false)][string]$ServiceId = '',
+    [Parameter(Mandatory=$false)][string]$ApiKey = '',
+    [Parameter(Mandatory=$false)][string]$CommitSha = '',
+    [Parameter(Mandatory=$false)][string]$RepositoryPath = (Get-Location).Path,
+    [switch]$ClearCache,
+    [switch]$WaitForProduction,
+    [switch]$VerifyOnly
+)
+
+$ErrorActionPreference = 'Stop'
+$modulePath = Join-Path $PSScriptRoot '..\ops\RenderDeploy.psm1'
+Import-Module $modulePath -Force
+
+if ([string]::IsNullOrWhiteSpace($ServiceId)) {
+    if (-not [string]::IsNullOrWhiteSpace($env:RENDER_SERVICE_ID)) {
+        $ServiceId = $env:RENDER_SERVICE_ID
+    } else {
+        $ServiceId = Read-Host 'Render service ID (srv-...)'
+    }
+}
+
+if ([string]::IsNullOrWhiteSpace($CommitSha)) {
+    $CommitSha = Get-GitCommitSha -RepositoryPath $RepositoryPath
+}
+
+if ($VerifyOnly) {
+    Test-RenderProduction -ExpectedCommit $CommitSha
+    exit 0
+}
+
+Invoke-RenderDeploy -ServiceId $ServiceId -ApiKey $ApiKey -RepositoryPath $RepositoryPath -CommitSha $CommitSha -ClearCache:$ClearCache -WaitForProduction:$WaitForProduction
