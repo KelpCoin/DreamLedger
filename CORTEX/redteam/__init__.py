f4d9955b5fe43e6feea08df35ffa81763543a4ee
KelpCoin <josharchibald89@gmail.com>
2026-09-12 15:45:15 +1200

    Wire canonical B01-B24 red-team package

--- CORTEX/redteam/__init__.py
+++ CORTEX/redteam/__init__.py
@@ -1,5 +1,6 @@
-"""Deterministic adversarial proposal checks, extension modules B13-B24."""
-
+"""DreamLedger adversarial red-team belt, B01-B24."""
+from .belt import BELT_VERSION,run_belt
 from .belt_extension import run_extension_belt
-
-__all__ = ["run_extension_belt"]
+from .contracts import BeltResult,Finding,ModuleResult,Verdict
+from .modules import REGISTRY,MODULE_ORDER
+__all__=['BELT_VERSION','run_belt','run_extension_belt','BeltResult','Finding','ModuleResult','Verdict','REGISTRY','MODULE_ORDER']
