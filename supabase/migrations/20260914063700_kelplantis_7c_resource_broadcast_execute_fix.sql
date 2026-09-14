992c4f4eb8ef50246725326f23a9a239da77a93f
KelpCoin <josharchibald89@gmail.com>
2026-09-14 18:34:46 +1200

    lock 7C internal broadcast trigger execute

---
+++ supabase/migrations/20260914063700_kelplantis_7c_resource_broadcast_execute_fix.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.kelplantis_broadcast_resource_change() from anon, authenticated, service_role, public;
+grant execute on function public.kelplantis_broadcast_resource_change() to postgres;
