f0ebe2a8116971babd71b9ab1fd4e837f81c97cc
KelpCoin <josharchibald89@gmail.com>
2026-09-24 10:11:14 +1200

    Point physical QR generator at dynamic short URL

--- IP/QR/generate_qr.py
+++ IP/QR/generate_qr.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import qrcode
 from qrcode.image.svg import SvgPathImage
 
-TARGET = 'https://buy.stripe.com/28EcN54zraG13M3g3idwc1t'
+TARGET = 'https://dreamledger.org/q/overpaying'
 OUT = Path(__file__).with_name('DreamLedger-Billboard-QR.svg')
 
 qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=8, border=4)
